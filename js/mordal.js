3f7f370775707d715b634d453cfefb76da6d1158
TDS Graphics <54712534+tdsgraphx@users.noreply.github.com>
2020-11-12 12:42:43 +0200

    Add files via upload
    initial commit

---
+++ js/mordal.js
@@ -0,0 +1,87 @@
+// Get the modal project 1
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 2
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 3
+var modal = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 4
+var modal = document.getElementById('id04');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 5
+var modal = document.getElementById('id05');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 6
+var modal = document.getElementById('id06');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 7
+var modal = document.getElementById('id07');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the modal project 8
+var modal = document.getElementById('id08');
+
+// When the user clicks anywhere outside of the modal, close it
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
